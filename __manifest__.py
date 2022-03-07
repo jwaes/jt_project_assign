a5854899141c91664b0a683816a8f6bad973a67f
jwaes <jaco.waes@gmail.com>
2022-03-07 12:43:45 +0000

    Added license to manifest

--- __manifest__.py
+++ __manifest__.py
@@ -8,6 +8,7 @@
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
+    "license": "AGPL-3",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
