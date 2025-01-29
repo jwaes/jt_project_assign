8303047aed747c1134370faa52d8db912f5fe57e
jwaes <jaco.waes@gmail.com>
2025-01-29 14:00:39 +0000

    Branch 18.0

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Project Management',
-    'version': '2.0',
+    'version': '18.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ["project"],
